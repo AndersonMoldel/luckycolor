dbc8282a9f8328177317dece0161720eabf8895f
AndersonMoldel <104338148+AndersonMoldel@users.noreply.github.com>
2026-01-13 05:32:45 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "../App"
 
-const rootElement = document.getElementById("root")
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to")
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
